4880635179968f40516e80e4c520f4e61e3476e3
rodrigoaleixoff <101883158+rodrigoaleixoff@users.noreply.github.com>
2025-06-03 13:32:45 -0300

    cors-installed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,11 @@
 import express from "express"
+import cors from "cors"
 
 import loginRouter from "./routes/loginRouter.js"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use("/login", loginRouter)

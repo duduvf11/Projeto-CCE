7f42ddc68f2b5224726cda972f759d0b99c98b28
rodrigoaleixoff <101883158+rodrigoaleixoff@users.noreply.github.com>
2025-06-03 13:30:02 -0300

    index-att

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,16 @@
-import express, { json } from "express"
+import express from "express"
 
-import loginRouter from "./routes/login.js"
+import loginRouter from "./routes/loginRouter.js"
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/login", loginRouter)
+
+app.get("/", (req, res) => {
+  res.json({msg: "ok"})
+})
 
 app.listen(3000, () => {
   console.log("Rodando na porta 3000")

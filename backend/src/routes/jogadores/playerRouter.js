f0a33160e8151ee5dab24a3156cb03c011ecfdfd
rodrigoaleixoff <101883158+rodrigoaleixoff@users.noreply.github.com>
2025-06-12 14:52:10 -0300

    controller and service - jogadores

---
+++ backend/src/routes/jogadores/playerRouter.js
@@ -0,0 +1,16 @@
+import {Router} from "express"
+
+const router = Router()
+
+//Lista de jogadores
+//router.get("/")
+
+//Jogador especifico
+
+//Novo Jogador
+
+//Atualizar jogador
+
+//Deletar jogador
+
+export default router
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ import cookieParser from "cookie-parser"
 import createUserRouter from "./routes/usuarios/createUserRouter.js"
 import loginUserRouter from "./routes/usuarios/loginUserRouter.js"
 import detailUserRouter from "./routes/usuarios/detailUserRouter.js"
-import teamsUserRouter from "./routes/times/teamsRouter.js"
+import teamsRouter from "./routes/times/teamsRouter.js"
+import playerRouter from "./routes/jogadores/playerRouter.js"
 
 const app = express()
 
@@ -16,7 +17,8 @@ app.use(cookieParser())
 app.use("/create", createUserRouter)
 app.use("/login", loginUserRouter)
 app.use("/me", detailUserRouter)
-app.use("/team", teamsUserRouter)
+app.use("/team", teamsRouter)
+app.use("/player", playerRouter)
 
 app.listen(3000, () => {
   console.log("Rodando na porta 3000")

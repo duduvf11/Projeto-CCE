66a5d83ef5d6cda5f6e2bd6d44fc4e4499808d82
rodrigoaleixoff <101883158+rodrigoaleixoff@users.noreply.github.com>
2025-06-12 11:02:21 -0300

    getTema Ready

--- backend/src/routes/times/teamsRouter.js
+++ backend/src/routes/times/teamsRouter.js
@@ -2,9 +2,14 @@ import { Router } from "express";
 import { auth } from "../../middlewares/auth.js";
 
 import { CreateTeam } from "../../controller/times/CreateTeamController.js";
+import { GetTeamController } from "../../controller/times/GetTeamsController.js";
 
 const router = Router()
 
+//Novo time
 router.post("/", auth, new CreateTeam().handle)
+
+//Times do usuario
+router.get("/", auth, new GetTeamController().handle)
 
 export default router
--- backend/src/service/times/GetTeamService.js
+++ backend/src/service/times/GetTeamService.js
@@ -1,8 +1,26 @@
 import prismaClient from "../../prisma/index.js"
 
 class GetTeamService{
-  async execute(){
+  async execute({userId}){
+    
+    try{
 
+      console.log(userId)
+
+      const getTeam = await prismaClient.time.findMany({
+        where: {
+          usuario: parseInt(userId)
+        }
+      })
+
+      if (!getTeam) return 0
+
+      return getTeam
+
+    } catch(err){
+      console.error(err)
+      return null
+    }
   }
 }
 
